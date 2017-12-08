d0fcf33e4a2575cfefbb218a52cfc0bd103838d1
connorcranston <connorcranston@gmail.com>
2017-12-07 19:42:09 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// console.log("Ready for javascript")
+
+var oper1=24+0
+var oper2 = 24 - 0
+var oper3 = 24 * 1
+var oper4 = 24 / 1
+
+// console.log(oper1)
+
+// console.log(5 !== 3)
+
+var op1 = 6 % 3
+var op2 = 10 % 2
+var op3 = 5 % 2
+
+// console.log(op1)
+// console.log(op2)
+// console.log(op3)
+// console.log(op2 !== op3)  //should say true
+// console.log(op1 == op3)  //shoudl say false
+
+var string1 = "Test Grades as follows"
+var string2 = "Student1: 96"
+var string3 = "Comment: \"None\""
+
+// console.log(string1+"\n \t"+string2+"\n \t"+string3)
+
+var numbers =[0,10,20,30,40,50,60,70,80,90,100]
+
+// console.log(numbers[2]+numbers[4])
+var me = {
+    first: "Connor",
+    last: "Cranston",
+    age: 25,
+    food: "coconut",
+    sex: "male",
+}
+
+// console.log('Hi my name is ${me.first} ${me.last}. I am ${me.age} years old. I am a ${me.male} human and I like ${me.coconut}.')
+// var num = 1
+// while(num < 10){
+//    if(num % 2 == 0){
+//        console.log(num)
+//    }
+//    num++
+// }
+
+// for(var i=1; i<=10; i++){
+//    if(i%2 == 1)
+//    console.log(i)
+//    }
+// }
+
+for(var i=1; i<=10; i+=2){
+    console.log(i)
+}
